8e9080ca4cf5dd6cf2da31d99bbcb274414e832e
marketing1206 <kdung1206@gmail.com>
2026-07-04 23:32:21 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base: '/marketing-report/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
